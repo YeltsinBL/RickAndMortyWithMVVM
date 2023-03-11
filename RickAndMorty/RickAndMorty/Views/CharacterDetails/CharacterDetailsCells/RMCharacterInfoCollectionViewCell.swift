4531af58a3302fd1b4922367af934fc4c6d8beb1
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-10 19:51:21 -0500

    Creación de la Cell del Detail para mostrar la información

--- RickAndMorty/RickAndMorty/Views/CharacterDetails/CharacterDetailsCells/RMCharacterInfoCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/Views/CharacterDetails/CharacterDetailsCells/RMCharacterInfoCollectionViewCell.swift
@@ -11,11 +11,48 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     /// Identificador de la Cell
     static let cellIdentifer = "RMCharacterInfoCollectionViewCell"
     
+    /// Valor de la Cell
+    private let valueLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Tierra"
+        label.font = .systemFont(ofSize: 22, weight: .light)
+        return label
+    }()
+    /// Titulo de la Cell
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Ubicacion"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        return label
+    }()
+    /// Icono de la Cell
+    private let iconImageView: UIImageView = {
+       let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(systemName: "globe.americas")
+        icon.contentMode = .scaleAspectFit
+        return icon
+    }()
+    /// Contiene el titulo de la Cell
+    private let titleContainerView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    
     //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.addCustomSubViews(titleContainerView, valueLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
+        setUpConstraints()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -23,13 +60,38 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     
     //MARK: - Func
     
+    /// Cuando se reutilice la cell
     override func prepareForReuse() {
         super.prepareForReuse()
+//        valueLabel.text = nil
+//        titleLabel.text = nil
+//        iconImageView.image = nil
     }
     
     /// Configurar los constraints
     private func setUpConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleContainerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleContainerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+            
+            titleLabel.leftAnchor.constraint(equalTo: titleContainerView.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: titleContainerView.rightAnchor),
+            titleLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+            
+            iconImageView.heightAnchor.constraint(equalToConstant: 30),
+            iconImageView.widthAnchor.constraint(equalToConstant: 30),
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35),
+            iconImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            
+            valueLabel.heightAnchor.constraint(equalToConstant: 30),
+            valueLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 10),
+            valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 36),
+            
+        ])
     }
     
     /// Recibir el ViewModel para asignarlo a los elementos de la Cell
