b98552de68878f8e055ced809b98a4a9df52ed11
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-03-13 20:03:29 -0500

    Diseño y mostrar información de los episodios del personaje en el detalle

--- RickAndMorty/RickAndMorty/Views/CharacterDetails/CharacterDetailsCells/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/RickAndMorty/Views/CharacterDetails/CharacterDetailsCells/RMCharacterEpisodeCollectionViewCell.swift
@@ -11,11 +11,37 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     /// Identificador de la Cell
     static let cellIdentifer = "RMCharacterEpisodeCollectionViewCell"
     
+    private let seasonLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+    private let nameLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        label.textAlignment = .center
+        return label
+    }()
+    private let airDateLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        label.textAlignment = .center
+        return label
+    }()
+    
     //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .secondarySystemBackground
+        contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.borderWidth = 2
+        contentView.layer.borderColor = UIColor.systemBlue.cgColor
+        contentView.addCustomSubViews(seasonLabel, nameLabel, airDateLabel)
+        setUpConstraints()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -25,19 +51,39 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        seasonLabel.text = nil
+        nameLabel.text = nil
+        airDateLabel.text = nil
     }
     
     /// Configurar los constraints
     private func setUpConstraints() {
-        
+        NSLayoutConstraint.activate([
+            seasonLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            seasonLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant:  10),
+            seasonLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant:  -10),
+            seasonLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3),
+            
+            nameLabel.topAnchor.constraint(equalTo: seasonLabel.bottomAnchor),
+            nameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant:  10),
+            nameLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant:  -10),
+            nameLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3),
+            
+            airDateLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor),
+            airDateLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant:  10),
+            airDateLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant:  -10),
+            airDateLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3),
+        ])
     }
     
     /// Recibir el ViewModel para asignarlo a los elementos de la Cell
     /// - Parameter characterEpisodeCollectionViewCellViewModel: Recibe los datos del ViewModel
     public func configure(with characterEpisodeCollectionViewCellViewModel: RMCharacterEpisodeCollectionViewCellViewModel){
         // Obtenemos los datos de las propiedades del protocolo
-        characterEpisodeCollectionViewCellViewModel.registerForData { data in
-            print("Datos: \(String(describing: data))")
+        characterEpisodeCollectionViewCellViewModel.registerForData { [weak self] data in
+            self?.seasonLabel.text = "Episodio " + data.episode
+            self?.nameLabel.text = data.name
+            self?.airDateLabel.text = "Fecha Emision " + data.air_date
         }
         characterEpisodeCollectionViewCellViewModel.fetchEpisode()
     }
